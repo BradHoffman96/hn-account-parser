a9b0dadc45ac9cb87f70dc2429f2830f30256815
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-09 17:30:40 -0400

    receiving comment items and storing them in array

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,12 +19,27 @@ type Account struct {
 	Submitted []int  `json:"submitted"`
 }
 
+//Comment struct for HN
+type Comment struct {
+	ID     int    `json:"id"`
+	By     string `json:"by"`
+	Type   string `json:"type"`
+	Time   int    `json:"time"`
+	Text   string `json:"text"`
+	Parent int    `json:"parent"`
+	Kids   []int  `json:"kids"`
+	URL    string `json:"url"`
+}
+
+var comments []Comment
 var client = &http.Client{Timeout: 10 * time.Second}
 
 func main() {
 	account := Account{}
 	getAccount("jandrewrogers", &account)
-	fmt.Println(account.Submitted)
+	getComments(account)
+
+	fmt.Println(len(comments))
 }
 
 func getAccount(id string, target interface{}) error {
@@ -35,4 +52,25 @@ func getAccount(id string, target interface{}) error {
 	defer resp.Body.Close()
 
 	return json.NewDecoder(resp.Body).Decode(target)
+}
+
+func getComments(account Account) {
+	for _, item := range account.Submitted {
+		url := []string{"https://hacker-news.firebaseio.com/v0/item/", strconv.Itoa(item), ".json?print=pretty"}
+
+		resp, err := client.Get(strings.Join(url, ""))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		comment := Comment{}
+		err = json.NewDecoder(resp.Body).Decode(&comment)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(comment.Text)
+
+		comments = append(comments, comment)
+	}
 }
